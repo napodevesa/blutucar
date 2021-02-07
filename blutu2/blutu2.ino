8579c109d896842d3367a57a80f7ad55272ccb98
ndev <napodevesa@gmail.com>
2021-02-07 23:28:09 +0100

    add file

---
+++ blutu2/blutu2.ino
@@ -0,0 +1,100 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial miBT (10, 11);
+
+char DATO = 0;
+int LEDROJO = 2;
+int LEDVERDE = 3;
+
+// L298N Control Pins
+const int LeftMotorForward = 4;
+const int LeftMotorBackward = 5;
+const int RightMotorForward = 6;
+const int RightMotorBackward = 7;
+
+
+void setup (){
+
+  miBT.begin (38400);
+  pinMode (LEDROJO, OUTPUT);
+  pinMode (LEDVERDE, OUTPUT);
+
+    // Set L298N Control Pins as Output
+  pinMode(RightMotorForward, OUTPUT);
+  pinMode(LeftMotorForward, OUTPUT);
+  pinMode(LeftMotorBackward, OUTPUT);
+  pinMode(RightMotorBackward, OUTPUT);
+}
+
+
+void loop (){
+
+  if (miBT.available())     //lee BT y envia a Arduino
+    DATO =  miBT.read();
+   
+      if (DATO== '1')
+      {
+      moveStop() ;
+      }
+      
+      if (DATO== '2')
+      {
+      moveForward()  ;
+      }
+
+       if (DATO== '3')
+      {
+      moveBackward();
+      }
+
+       if (DATO== '4')
+      {
+      turnRight()  ;
+      }
+
+      if (DATO== '5')
+      {
+      turnLeft()  ;
+      }
+    
+}
+
+void moveStop()       // Move Stop Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, LOW);
+}
+
+void moveForward()    // Move Forward Function for Motor Driver.
+{
+    digitalWrite(RightMotorForward, HIGH);
+    digitalWrite(RightMotorBackward, LOW);
+    digitalWrite(LeftMotorForward, HIGH);
+    digitalWrite(LeftMotorBackward, LOW);
+}
+
+void moveBackward()   // Move Backward Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(RightMotorBackward, HIGH);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, HIGH);
+}
+
+void turnRight()      // Turn Right Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, LOW);
+  digitalWrite(RightMotorBackward, HIGH);
+  digitalWrite(LeftMotorForward, HIGH);
+  digitalWrite(LeftMotorBackward, LOW);
+}
+
+void turnLeft()       // Turn Left Function for Motor Driver.
+{
+  digitalWrite(RightMotorForward, HIGH);
+  digitalWrite(RightMotorBackward, LOW);
+  digitalWrite(LeftMotorForward, LOW);
+  digitalWrite(LeftMotorBackward, HIGH);
+}
